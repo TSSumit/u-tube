5479763b9e548567daaa66b807ae396522095ac3
TSSumit <tikonesum2001@gmail.com>
2024-06-14 22:28:03 +0530

    video play improvement

--- src/components/VideoInfo.js
+++ src/components/VideoInfo.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BiLike } from 'react-icons/bi';
+import { BiDislike, BiLike } from 'react-icons/bi';
 import { Link } from 'react-router-dom';
 
 const VideoInfo = ({data}) => {
@@ -19,10 +19,10 @@ const formatCount = (count) => {
   const channelId=data[1]?.id;
   return (
     <div className="flex-row justify-center items-center p-4 w-[100%]">
-      <div className="mb-4">
-        <h2 className="text-xl font-semibold">{data[0]?.snippet?.title}</h2>
-        <div className='h-[40px] border-black border-2 flex justify-between'>
-            <div className='flex'>
+      <div className="mb-2">
+        <h2 className="text-xl -mt-4 mb-2 font-bold">{data[0]?.snippet?.title}</h2>
+        <div className='h-[40px]  flex justify-between'>
+            <div className='w-full flex justify-between '>
                 <Link key={channelId} to={"/"+channelId} className='flex h-[100%]'>
                     <img src={data[1]?.snippet?.thumbnails?.medium?.url} alt="Channel Logo" className='h-[40px] w-[40px] rounded-full mr-2' />
                     <div className='flex-row'>
@@ -30,41 +30,25 @@ const formatCount = (count) => {
                         {data[1]?.snippet?.title}
                       </li>
                       <li className='text-[0.85rem] text-gray-800 p-0 list-none leading-tight mb-0'>
-                        {formatCount(data[0]?.statistics?.likeCount)}
-                        <span>
-                          <BiLike />
-                        </span>
-                      </li>
-                      <li className='text-[0.85rem] text-gray-800 p-0 list-none leading-tight mb-0'>
-                        {formatCount(data[0]?.statistics?.likeCount)}
-                        <span>
-                          <BiLike />
-                        </span>
+                        {formatCount(data[1]?.statistics?.subscriberCount)}M subscribers
                       </li>
                     </div>
                 </Link>
-                <button className=' bg-gray-50 ml-4 border-black border-[1.5px] rounded-full px-2'>Subscribe</button>
               </div> 
-              <div className='flex'>
-                  <li className='text-[1rem] p-0 font-bold list-none leading-tight mb-0'>
-                    {data[1]?.snippet?.title}
-                  </li>
-                  <li className='text-[0.85rem] text-gray-800 p-0 list-none leading-tight mb-0'>
-                    {formatCount(data[1]?.statistics?.subscriberCount)}M subscribers
-                  </li>
+              <div className='flex mx-3'>
+                <button className='bg-gray-50 mr-3 font-semibold text-center border-black border-[1px] rounded-full p-2 flex items-center justify-center'>Subscribe</button>
+                <div className='flex'>
+                  <button className='flex items-center justify-center h-full text-[0.85rem] text-center font-bold text-gray-800 p-2 list-none leading-tight mb-0 align-middle border-black border-[1px] rounded-l-full'>
+                    {formatCount(data[0]?.statistics?.likeCount)}
+                    <BiLike className='w-[25px] h-[25px]' />
+                  </button>
+                  <button className='flex items-center justify-center h-full text-[0.85rem] text-gray-800 list-none leading-tight mb-0 p-2 border-black border-[1px] border-l-0 rounded-r-full'>
+                    <BiDislike className='w-[25px] h-[25px]' />
+                  </button>
+                </div>
               </div>
             <div className='flex'></div>
         </div>
-        {/* <p>Views: {data[0]?.statistics?.viewCount} views</p>
-        <p>date: {data[0]?.snippet?.publishedAt} date</p>
-        <p>Likes: {data[0]?.statistics?.likeCount}</p>
-        <p>Comments: {data[0]?.statistics?.commentCount}</p> */}
-        {/* <p className="text-sm text-gray-600">{data[0]?.snippet?.description}</p> */}
-      </div>
-
-      <div className="mb-4">
-        <img src={data[1]?.snippet?.thumbnails?.medium?.url} alt="Channel Logo" />
-        <h1 className="text-xl font-semibold">{data[1]?.snippet?.title}</h1>  
       </div>
     </div>
   );
